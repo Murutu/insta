e0dcd7999e05241671a05518a956d3f40d6cdb56
murutu <murutupeter@gmail.com>
2021-12-03 12:09:16 +0300

    jwt secret

--- components/Header.jsx
+++ components/Header.jsx
@@ -49,19 +49,6 @@ function Header() {
             <Image src="/insta-logo.png" layout="fill" objectFit="contain" />
           </div>
 
-          <div className="max-w-xs">
-            <div className="relative mt-1 p-3 rounded-md">
-              {/* <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
-                <SearchIcon className="h-5 w-5 text-gray-500" />
-              </div>
-              <input
-                className="bg-gray-50 w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black"
-                type="text"
-                placeholder="Search"
-              /> */}
-            </div>
-          </div>
-
           <div className="flex items-center justify-end space-x-6">
             <MenuIcon
               className="h-6 md:hidden cursor-pointer"
